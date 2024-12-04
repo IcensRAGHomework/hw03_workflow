4f81a94324d80ad9fd52e14082309a8b9c54cb25
r17652001 <r17652001@gmail.com>
2024-12-04 19:48:26 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -1,7 +1,38 @@
-def add(a, b):
-    """Return the sum of a and b.  """
-    return a + b
+import datetime
+import chromadb
+import traceback
 
-def sub(a, b):
-    """Return the sum of a and b.  """
-    return a - b
+from chromadb.utils import embedding_functions
+
+from model_configurations import get_model_configuration
+
+gpt_chat_version = 'gpt-4o'
+gpt_emb_config = get_model_configuration(gpt_chat_version)
+
+dbpath = "./"
+
+def generate_hw01():
+    pass
+    
+def generate_hw02(question, city, store_type, start_date, end_date):
+    pass
+    
+def generate_hw03(question, store_name, new_store_name, city, store_type):
+    pass
+    
+def demo(question):
+    chroma_client = chromadb.PersistentClient(path=dbpath)
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key = gpt_emb_config['api_key'],
+        api_base = gpt_emb_config['api_base'],
+        api_type = gpt_emb_config['openai_type'],
+        api_version = gpt_emb_config['api_version'],
+        deployment_id = gpt_emb_config['deployment_name']
+    )
+    collection = chroma_client.get_or_create_collection(
+        name="TRAVEL",
+        metadata={"hnsw:space": "cosine"},
+        embedding_function=openai_ef
+    )
+    
+    return collection
